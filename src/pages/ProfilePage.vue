bc59c35ae8b397f0fd2cd3dba68512c68d50a58d
Jeremy Fowler <jembyfowler@gmail.com>
2024-07-18 11:04:22 -0600

    rendering profile information

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,9 +1,14 @@
 <script setup>
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 import Pop from '../utils/Pop.js';
 import { profilesService } from '../services/ProfilesService.js';
 import { useRoute } from 'vue-router';
 import { logger } from '../utils/Logger.js';
+import { AppState } from '../AppState.js';
+
+const profile = computed(() => AppState.profile)
+
+const profileCoverImg = computed(() => `url(${profile.value?.coverImg})`)
 
 const route = useRoute()
 
@@ -23,8 +28,33 @@ async function getProfileById(profileId) {
 
 
 <template>
-  <h1>Profile Page</h1>
+  <!-- NOTE if there is a profile in the appstate, render the HTML for it -->
+  <!-- NOTE because this property is null on page load, we have to do this check -->
+  <div v-if="profile" class="container">
+    <section class="row">
+      <div class="col-12">
+        <div class="text-center cover-img">
+          <img :src="profile.picture" :alt="profile.name" class="profile-img">
+          <h1>{{ profile.name }}</h1>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.profile-img {
+  height: 35.6vh;
+}
+
+.cover-img {
+  height: 60vh;
+  background-image: v-bind(profileCoverImg);
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
