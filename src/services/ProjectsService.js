324a2407043dd7681d4ccbe8a3a03a38413825ea
Jeremy Fowler <jembyfowler@gmail.com>
2024-07-18 09:33:33 -0600

    got projects

---
+++ src/services/ProjectsService.js
@@ -0,0 +1,12 @@
+import { logger } from "../utils/Logger.js"
+import { api } from "./AxiosService.js"
+
+class ProjectsService {
+  async getProjects() {
+    const response = await api.get('api/projects')
+    logger.log('GOT PROJECTS 🖼️🎨🎭', response.data)
+  }
+
+}
+
+export const projectsService = new ProjectsService()
