d34bb303872e5e1660e0fb66ce66bf3b1afb15fe
Jeremy Fowler <jembyfowler@gmail.com>
2024-07-18 09:40:57 -0600

    dumped raw data onto page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 import Pop from '../utils/Pop.js';
 import { projectsService } from '../services/ProjectsService.js';
+import { AppState } from '../AppState.js';
 
+const projects = computed(() => AppState.projects)
 
 onMounted(() => { getProjects() })
 
@@ -23,6 +25,9 @@ async function getProjects() {
       <div class="col-12">
         <h1>Projects</h1>
       </div>
+    </section>
+    <section class="row">
+      {{ projects }}
     </section>
   </div>
 </template>
