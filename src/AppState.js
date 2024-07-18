6ac02051a3f289893e9fe008f70258fcb83a44fc
Jeremy Fowler <jembyfowler@gmail.com>
2024-07-18 10:51:15 -0600

    storing profile in appstate

--- src/AppState.js
+++ src/AppState.js
@@ -7,5 +7,7 @@ export const AppState = reactive({
   /** @type {import('./models/Account.js').Account} user info from the database*/
   account: null,
   /** @type {import('./models/Project.js').Project[]} */
-  projects: []
+  projects: [],
+  /** @type {import('./models/Profile.js').Profile} */
+  profile: null
 })
--- src/services/ProfilesService.js
+++ src/services/ProfilesService.js
@@ -1,3 +1,5 @@
+import { AppState } from "../AppState.js"
+import { Profile } from "../models/Profile.js"
 import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 
@@ -5,6 +7,8 @@ class ProfilesService {
   async getProfileById(profileId) {
     const response = await api.get(`api/profiles/${profileId}`)
     logger.log('GOT PROFILE 🕴️', response.data)
+    const profile = new Profile(response.data)
+    AppState.profile = profile
   }
 }
 
