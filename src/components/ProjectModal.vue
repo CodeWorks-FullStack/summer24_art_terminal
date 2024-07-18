463e26179e6437f5b3b7bc42b8e99c966b970fa9
Jeremy Fowler <jembyfowler@gmail.com>
2024-07-18 13:04:18 -0600

    displaying project in modal

--- src/components/ProjectModal.vue
+++ src/components/ProjectModal.vue
@@ -1,20 +1,30 @@
-<script setup></script>
+<script setup>
+import { computed } from 'vue';
+import { AppState } from '../AppState.js';
+
+const project = computed(() => AppState.activeProject)
+</script>
 
 
 <template>
   <div class="modal fade" id="projectModal" tabindex="-1" aria-labelledby="projectModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
+    <div class="modal-dialog modal-xl">
+      <div v-if="project" class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="projectModalLabel">Modal title</h1>
+          <h1 class="modal-title fs-5" id="projectModalLabel">{{ project.title }}</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          <div class="container">
+            <div class="row">
+              <div v-for="img in project.projectImgs" :key="img" class="col-12">
+                <img :src="img" alt="" class="img-fluid w-100">
+              </div>
+            </div>
+          </div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
